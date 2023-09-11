63550792a06efa9961e59fd599d666ae066afcdc
rogeriodona <rogerio.don@outlook.com>
2023-09-11 14:18:07 -0300

    ajustes

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ console.log("funcionou?")
 const convertButton = document.querySelector(".convert-button")
 console.log("convert-button")
 
-function convertValues(){
-    console.log("convert-button")
-    const inputCurrencyValue = document.querySelector (".input-Currency").value
-    console.log(inputCurrencyValue)
+function convertValues (){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert1 = document.querySelector(".input-currency-to-convert") //valor em real
+    const currencyValueToConverted = document.querySelector(".currency-value") //outras moedas
+
+
+    const dolarToday = 4.93
+
+    const convertedValue = inputCurrencyValues / dolarToday
+
+    currencyValueToConvert.innerHTML = inputCurrencyValue
+    currencyValueToConverted.innerHTML = convertedValue
+
+
+
+    console.log(convertedValue)
+
 }
 
 
 convertButton.addEventListener("click", convertValues)
-
 
