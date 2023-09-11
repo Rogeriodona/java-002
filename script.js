f1ca3c346943fe13ecc1a38f2702855e3471e366
rogeriodona <rogerio.don@outlook.com>
2023-09-11 17:24:36 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 console.log("funcionou?")
 
 const convertButton = document.querySelector(".convert-button")
-console.log("convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues (){
+
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert1 = document.querySelector(".input-currency-to-convert") //valor em real
-    const currencyValueToConverted = document.querySelector(".currency-value") //outras moedas
-
-
-    const dolarToday = 4.93
-
-    const convertedValue = inputCurrencyValues / dolarToday
-
-    currencyValueToConvert.innerHTML = inputCurrencyValue
-    currencyValueToConverted.innerHTML = convertedValue
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valo em real //
+    const currencyValueConverted = document.querySelector(".currency-value")  // outras moedas//
 
 
 
-    console.log(convertedValue)
+    const dolarToday = 4.9
+    const euroToday = 5.3
+
+
+    if(currencySelect.value == "dolar"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-USD", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+
+
+
+    if(currencySelect.value == "euro"){
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style:
+                "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style:
+            "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
 
 }
 
 
 convertButton.addEventListener("click", convertValues)
-
